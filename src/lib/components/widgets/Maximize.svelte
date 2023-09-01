33dac5ea6957b8c43eebb4986d9ffe1b10333313
juani <jivila@outlook.com>
2023-09-01 17:33:06 -0300

    Maximize feature working without loading data again when clicking maximize button

--- src/lib/components/widgets/Maximize.svelte
+++ src/lib/components/widgets/Maximize.svelte
@@ -1,24 +1,22 @@
-<script>
-	import { selectedWidgetMaximize } from '$lib/stores/widgets'
-	import Widget from './Widget.svelte'
-
-	$: widget = $selectedWidgetMaximize
+<script lang="ts">
+    import {selectedWidgetMaximize} from "$lib/stores/widgets";
+    import Widget from "$lib/components/widgets/Widget.svelte";
 </script>
 
-{#if widget}
-	<div class="widget-maximize">
-		<Widget widget={$widget} />
-	</div>
+{#if $selectedWidgetMaximize?.widget}
+    <div class="widget-maximize">
+        <Widget widget={$selectedWidgetMaximize?.widget}/>
+    </div>
 {/if}
 
 <style>
-	.widget-maximize {
-		z-index: 9999;
-		position: fixed;
-		top: 0;
-		left: 0;
-		width: 100vw !important;
-		height: 100vh !important;
-		border-radius: 0;
-	}
+    .widget-maximize {
+        z-index: 9999;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw !important;
+        height: 100vh !important;
+        border-radius: 0;
+    }
 </style>
--- src/lib/components/widgets/base/Rest.svelte
+++ src/lib/components/widgets/base/Rest.svelte
@@ -1,34 +1,53 @@
 <script lang="ts">
-	import { getData } from '$lib/services/getData'
-	import { getContext } from 'svelte'
+    import {getData} from "$lib/services/getData";
+    import {getContext} from "svelte";
+    import {selectedWidgetMaximize} from "$lib/stores/widgets";
 
-	let widget: any = getContext('widget')
+    let widget: any = getContext("widget");
 
-	let widgetActions: any = getContext('widgetActions')
+    let widgetActions: any = getContext("widgetActions");
 
-	const url = $widget.query_slug.url
-	const method = $widget.query_slug.method
-	const body = $widget.query_slug.body
+    const url = $widget.query_slug.url;
+    const method = $widget.query_slug.method;
+    const body = $widget.query_slug.body;
 
-	let data: any
-	async function fetchData() {
-		data = getData(url, method)
-	}
+    let data: any;
 
-	if (!$widgetActions.find((action: any) => action.name === 'reloadFetchData')) {
-		const actions = $widgetActions
-		actions.push({
-			name: 'reloadFetchData',
-			action: () => fetchData()
-		})
-		$widgetActions = actions
-	}
+    async function fetchData() {
+        data = ($selectedWidgetMaximize?.data) ? $selectedWidgetMaximize.data : getData(url, method);
+    }
 
-	fetchData()
+    function storeData() {
+        selectedWidgetMaximize.set({
+            data, widget: $widget
+        });
+    }
+
+
+    if (!$widgetActions.find((action: any) => action.name === "reloadFetchData")) {
+        const actions = $widgetActions;
+        actions.push({
+            name: "reloadFetchData",
+            action: () => fetchData()
+        });
+        $widgetActions = actions;
+    }
+    if (!$widgetActions.find((action: any) => action.name === "maximizeWidget")) {
+        const actions = $widgetActions;
+        actions.push({
+            name: "maximizeWidget",
+            action: () => storeData()
+        });
+        $widgetActions = actions;
+    }
+
+
+    fetchData();
+
 </script>
 
 {#await data}
-	<p>loading</p>
+    <p>loading</p>
 {:then data}
-	<slot {data} />
+    <slot {data}/>
 {/await}
