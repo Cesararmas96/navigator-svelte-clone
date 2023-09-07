1227368ea886faa40bf439137b8142cf87751ba4
mendozajoser <mendozajoser70@gmail.com>
2023-09-07 09:21:08 -0500

    wip

--- src/lib/components/widgets/toolbar/ExportData.svelte
+++ src/lib/components/widgets/toolbar/ExportData.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import { DropdownItem, Button, Modal, Select, Input, Checkbox } from 'flowbite-svelte'
+	import { DropdownItem, Modal, Select, Input, Checkbox, Label } from 'flowbite-svelte'
 	import Icon from '$lib/components/common/Icon.svelte'
 	import { createEventDispatcher, getContext } from 'svelte'
 
 	const dispatch = createEventDispatcher()
 
 	let widgetActions: any = getContext('widgetActions')
-	let walletModal = false
+	let openExportData = false
 	let selected: any
 	let formats = [
-		{ value: 'csv', name: 'CSV' },
-		{ value: 'xlsx', name: 'XLSX' },
-		{ value: 'pdf', name: 'PDF' }
+		{ value: 'csv', name: 'CSV', icon: 'bi:filetype-csv' },
+		{ value: 'xlsx', name: 'XLSX', icon: 'bi:filetype-xlsx' },
+		{ value: 'pdf', name: 'PDF', icon: 'bi:filetype-pdf' }
 	]
 
 	$: exportData = $widgetActions.find((action: any) => action.name === 'exportData')
 
 	function onClick() {
-		walletModal = true
+		openExportData = true
 		// dispatch('itemClick', { click: true })
 	}
 </script>
@@ -28,14 +28,25 @@
 	</DropdownItem>
 {/if}
 
-<Modal title="Export Data" bind:open={walletModal} size="xs">
+<Modal title="Export Data" bind:open={openExportData} size="xs">
 	<p class="text-sm font-normal text-gray-500 dark:text-gray-400">Choose file export format.</p>
 	<span class="my-4 space-y-3">
-		<Select class="mt-2" items={formats} bind:value={selected} />
-		<!-- <select name="select">
-			<option value="value1">Value 1</option>
-			<option value="value2" selected>Value 2</option>
-			<option value="value3">Value 3</option>
-		</select> -->
+		<!-- <Select class="mt-2" items={formats} bind:value={selected} /> -->
+
+		<ul class="my-4 space-y-3">
+			{#each formats as format}
+				<li>
+					<a
+						href="/"
+						class="group flex items-center rounded-lg bg-gray-50 p-3 text-base font-bold text-gray-900 hover:bg-gray-100 hover:shadow dark:bg-gray-600 dark:text-white dark:hover:bg-gray-500"
+					>
+						<span class="ml-3 flex-1 items-center whitespace-nowrap">
+							<Icon icon={format.icon} size="18" />
+							{format.name}</span
+						>
+					</a>
+				</li>
+			{/each}
+		</ul>
 	</span>
 </Modal>
