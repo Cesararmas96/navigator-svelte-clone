1ea4eb25fa784d47f9acc40d5a14e06a5b8e126c
juani <jivila@outlook.com>
2023-09-20 14:49:35 -0300

    Close feature implemented

--- src/lib/components/widgets/toolbar/Close.svelte
+++ src/lib/components/widgets/toolbar/Close.svelte
@@ -1,9 +1,56 @@
 <script lang="ts">
-	import { Tooltip } from 'flowbite-svelte'
-	import Icon from '$lib/components/common/Icon.svelte'
+    import {Tooltip} from "flowbite-svelte";
+    import Icon from "$lib/components/common/Icon.svelte";
+    import {getData} from "$lib/services/getData";
+
+    import {getContext, setContext} from "svelte";
+    import type {Writable} from "svelte/store";
+    import {get} from "svelte/store";
+
+    const widget = getContext<Writable<any>>("widget");
+    const urlBase = import.meta.env.VITE_API_URL;
+    const endpoint = `${urlBase}/api/v2/widgets/${$widget.uid}`;
+    const method = "DELETE";
+    export let component;
+
+
+    const widgets = getContext<Writable<any>>("widgets");
+
+
+    console.log($widget);
+
+
+    // Temporal variable
+    const isClonedWidget = false;
+    const removeWidgetFromDOM = () => {
+
+        const widgetsToSet = $widgets.filter(widget => widget.uid !== $widget.uid);
+        console.log(widgetsToSet);
+        setContext("widgets", widgetsToSet);
+        
+
+    };
+
+
+    async function closeWidget() {
+        // Removes DOM element and DELETE request if not cloned
+        if (!isClonedWidget) {
+            try {
+                // const response = await getData(endpoint, method);
+                removeWidgetFromDOM();
+
+            } catch (e) {
+                console.log(e.message);
+            }
+        } else removeWidgetFromDOM();
+
+
+    }
 </script>
 
-<button class="icon btn hover:bg-light-100 dark:hover:bg-dark-200">
-	<Icon icon="tabler:x" size="18" />
+<button on:click={closeWidget} class="icon btn hover:bg-light-100 dark:hover:bg-dark-200">
+    <Icon icon="tabler:x" size="18"/>
 </button>
 <Tooltip>Close</Tooltip>
+
+
