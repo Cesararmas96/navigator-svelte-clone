73cf72f3d97f0e72e1a678469b88311ef9af7a8d
juani <jivila@outlook.com>
2023-10-02 12:43:22 -0300

    Profile view landscape improved

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -24,79 +24,77 @@
 
 
 {#await rawSession then session}
-
-    <Tabs>
+    <div style={"display: flex; flex-direction: row"}>
         <Profile session={session}/>
-        <TabItem>
+
+        <Tabs>
+            <TabItem>
 
 
-            <div slot="title" class="items-center gap-2 flex">
-                <Icon icon="mdi:user" size="20px"/>
-                Identities
-            </div>
-            <p>
-                <b>Profile:</b>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et
-                dolore magna aliqua.
-            </p>
-        </TabItem>
-        <TabItem>
+                <div slot="title" class="items-center gap-2 flex">
+                    <Icon icon="mdi:user" size="20px"/>
+                    Identities
+                </div>
+                <p>
+                    <b>Profile:</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua.
+                </p>
+            </TabItem>
+            <TabItem>
 
-            <div slot="title" class="flex items-center gap-2">
-                <Icon icon="mdi:world" size="20px"/>
-                Accounts
-            </div>
-
-
-            <p class="text-sm text-gray-500 dark:text-gray-400">
-                <b>Settings:</b>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et
-                dolore magna aliqua.
-            </p>
-        </TabItem>
+                <div slot="title" class="flex items-center gap-2">
+                    <Icon icon="mdi:world" size="20px"/>
+                    Accounts
+                </div>
 
 
-        <TabItem open>
-
-            <div slot="title" class="flex items-center gap-2">
-                <Icon icon="mdi:lock" size="20px"/>
-                Security
-            </div>
-
-
-            <div id="content">
+                <p class="text-sm text-gray-500 dark:text-gray-400">
+                    <b>Settings:</b>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua.
+                </p>
+            </TabItem>
 
 
-                <div class="p-5" data-simplebar>
-                    <div class="mx-auto max-sm">
-                        <div class="card mb-5 p-5 max-w-sm flex">
-                            <p class="pb-1">Current password</p>
-                            <input type="password" class="form-control mb-6 " placeholder="Password"/>
+            <TabItem open>
 
-                        </div>
-
-                        <div class="card mb-5 p-5 max-w-8xl mb-10">
-
-                            <p class="pb-1 px-3">New password</p>
-                            <input style="min-width: 2000px" type="password" name="password" bind:value={password}/>
-                            <PasswordStrength {password}/>
+                <div slot="title" class="flex items-center gap-2">
+                    <Icon icon="mdi:lock" size="20px"/>
+                    Security
+                </div>
 
 
-                            <p class="pb-1 px-3 pt-3">Confirm new password</p>
-                            <input type="password" class="form-control mb-6" placeholder="Password"/>
-
-                        </div>
-
+                <div id="content">
+                    <!--                    <div class="p-5" data-simplebar>-->
+                    <!--                        <div class="mx-auto max-sm">-->
+                    <div class="card mb-5 p-5 max-w-sm flex">
+                        <p class="pb-1">Current password</p>
+                        <input type="password" class="form-control mb-6 " placeholder="Password"/>
 
                     </div>
+
+                    <div class="card mb-5 p-5 max-w-8xl mb-10">
+
+                        <p class="pb-1 px-3">New password</p>
+                        <input style="min-width: 2000px" type="password" name="password" bind:value={password}/>
+                        <PasswordStrength {password}/>
+
+
+                        <p class="pb-1 px-3 pt-3">Confirm new password</p>
+                        <input type="password" class="form-control mb-6" placeholder="Password"/>
+
+                    </div>
+
+
+                    <!--                        </div>-->
+                    <!--                    </div>-->
                 </div>
-            </div>
 
 
-        </TabItem>
-    </Tabs>
-
-
+            </TabItem>
+        </Tabs>
+    </div>
 {/await}
