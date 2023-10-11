ed02923b18dc1909026fb79380580e34262b5d9c
mendozajoser <mendozajoser70@gmail.com>
2023-10-11 07:58:23 -0500

    Cards colors

--- src/lib/components/widgets/type/CardWidget/Card.svelte
+++ src/lib/components/widgets/type/CardWidget/Card.svelte
@@ -69,7 +69,7 @@
 
 	$: isLoading = $widget.instance_loading && drilldownOpen
 
-	const cardClass = `apiCard-content animate__animated animate__zoomIn card navigator-blue  ${
+	const cardClass = `apiCard-content animate__animated animate__zoomIn card  ${
 		card.class || 'navigator-blue'
 	} h-full overflow-hidden rounded-md border border-light-200 p-6 py-3 hover:-translate-y-0.5 hover:scale-100 hover:shadow-lg dark:border-dark-200 dark:bg-[#22313a]`
 </script>
