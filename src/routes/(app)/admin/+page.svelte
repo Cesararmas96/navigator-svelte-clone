6cbda7b4138d294857793cc1cb348164d1b9e13c
mendozajoser <mendozajoser70@gmail.com>
2023-10-23 19:56:57 -0500

    Wip admin

---
+++ src/routes/(app)/admin/+page.svelte
@@ -0,0 +1,190 @@
+<div>
+	<div>
+		<div class="faq-header h-52 px-5 py-10 text-center">
+			<h2 class="mb-1 text-2xl font-bold text-heading">Navigator Admin</h2>
+			<div class="mb-7 text-lg">
+				Navigator Admin makes it easier to build better web apps more quickly and with less code.
+			</div>
+
+			<form class="mx-auto max-w-md">
+				<input
+					type="text"
+					class="form-control h-11 rounded-md bg-white/75 text-center text-base placeholder:text-muted dark:bg-dark-100/50"
+					placeholder="Type keywords to find model..."
+				/>
+			</form>
+		</div>
+
+		<!-- <div class="mx-auto max-w-7xl p-5">
+			<div id="faq" class="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
+				<div class="card px-7 py-5">
+					<h3 class="mb-3 flex items-center font-bold text-heading">
+						<div class="flex-1 text-lg">Manage your users</div>
+						<i class="icon ml-auto text-2xl font-light">account_circle</i>
+					</h3>
+					<div>
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Users <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Users Groups <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+					</div>
+				</div>
+
+				<div class="card px-7 py-5">
+					<h3 class="mb-3 flex items-center font-bold text-heading">
+						<div class="flex-1 text-lg">Manage your Groups</div>
+						<i class="icon ml-auto text-2xl font-light">paid</i>
+					</h3>
+					<div>
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Groups <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+					</div>
+				</div>
+
+				<div class="card px-7 py-5">
+					<h3 class="mb-3 flex items-center font-bold text-heading">
+						<div class="flex-1 text-lg">Manage your organizations</div>
+						<i class="icon ml-auto text-2xl font-light">verified_user</i>
+					</h3>
+					<div>
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Organizations <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Organizations Groups <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+					</div>
+				</div>
+
+				<div class="card px-7 py-5">
+					<h3 class="mb-3 flex items-center font-bold text-heading">
+						<div class="flex-1 text-lg">Manage your Programs</div>
+						<i class="icon ml-auto text-2xl font-light">login</i>
+					</h3>
+					<div>
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Programs <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Programs Clients <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Programs Groups <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+					</div>
+				</div>
+
+				<div class="card px-7 py-5">
+					<h3 class="mb-3 flex items-center font-bold text-heading">
+						<div class="flex-1 text-lg">Manage your clients</div>
+						<i class="icon ml-auto text-2xl font-light">ads_click</i>
+					</h3>
+					<div>
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Clients <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Programs Clients <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Clients Groups <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+					</div>
+				</div>
+
+				<div class="card px-7 py-5">
+					<h3 class="mb-3 flex items-center font-bold text-heading">
+						<div class="flex-1 text-lg">Manage your modules</div>
+						<i class="icon ml-auto text-2xl font-light">apps</i>
+					</h3>
+					<div>
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Modules <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Modules Clients <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+
+						<a
+							href="#"
+							class="block flex items-center border-b border-dashed border-light-200 py-2 hover:text-link dark:border-dark-200 dark:hover:text-heading"
+							data-tb-modal="#modal-faq-details"
+							>Modules Groups <i class="icon ml-auto font-light text-muted">east</i>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div> -->
+
+		<!-- search -->
+
+		<div class="mx-auto max-w-7xl">
+			<div class="mb-2 px-3 text-muted">A total of 2 results</div>
+			<div id="search-results">
+				<a
+					href=""
+					class="mb-1 block rounded-md border border-light-200 bg-white px-4 py-3 hover:border-light-300 hover:bg-light-100/75 dark:border-none dark:bg-dark-100 dark:hover:bg-dark-200"
+				>
+					<div class="mb-1 truncate font-bold text-heading">Users</div>
+					<div class="truncate text-sm text-muted">Last update 2023-10-23</div>
+				</a>
+				<a
+					href=""
+					class="mb-1 block rounded-md border border-light-200 bg-white px-4 py-3 hover:border-light-300 hover:bg-light-100/75 dark:border-none dark:bg-dark-100 dark:hover:bg-dark-200"
+				>
+					<div class="mb-1 truncate font-bold text-heading">Users Groups</div>
+					<div class="truncate text-sm text-muted">Last update 2023-10-23</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
