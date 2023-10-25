3c9cd17f5d5c4b636e185c87f561da3ce2862d22
juani <jivila@outlook.com>
2023-10-25 14:51:41 -0300

    Allow programs by tentant checkpoint. Now working

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,22 +1,33 @@
 <script lang="ts">
-	import Card from '$lib/components/home/Card.svelte'
-	import { storePrograms } from '$lib/stores/programs'
-	import { storeUser } from '$lib/stores/session.js'
+    import Card from "$lib/components/home/Card.svelte";
+    import {storePrograms} from "$lib/stores/programs";
+    import {storeUser} from "$lib/stores/session.js";
+    import {goto} from "$app/navigation";
+    import Spinner from "$lib/components/common/Spinner.svelte";
+    import {filterPrograms, sortPrograms} from "$lib/helpers/programs";
 
-	export let data
+    export let data;
+    $storePrograms = data.programs;
+    $storeUser = data.user;
 
-	$storePrograms = data.programs
-	$storeUser = data.user
 
-	const allPrograms = data.programs.sort(function (program: any, program2: any) {
-		return program.program_name.localeCompare(program2.program_name)
-	})
+
+const allowedPrograms = sortPrograms(filterPrograms(data.programs, $storeUser.programs));
+
+// If there is only one program, redirect to it
+allowedPrograms.length === 1 && goto(`/${allowedPrograms[0].program_slug}`);
+
 </script>
 
-{#if data.programs}
-	<ul class="grid grid-cols-2 gap-4 p-4 sm:gap-5 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
-		{#each allPrograms as program}
-			<Card {program} />
-		{/each}
-	</ul>
+
+{#if allowedPrograms.length === 1}
+    <Spinner fullScreen={false}/>
+
+
+{:else if allowedPrograms}
+    <ul class="grid grid-cols-2 gap-4 p-4 sm:gap-5 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6">
+        {#each allowedPrograms as program}
+            <Card {program}/>
+        {/each}
+    </ul>
 {/if}
