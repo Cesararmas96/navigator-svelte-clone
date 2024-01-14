f4fbe66858bef6fe478794c69f3ede535763d566
mendozajoser <mendozajoser70@gmail.com>
2024-01-14 12:50:39 -0500

    refactor

--- src/lib/components/widgets/type/formBuilder/formBuilder.svelte
+++ src/lib/components/widgets/type/formBuilder/formBuilder.svelte
@@ -7,12 +7,15 @@
 	import { Button, Alert } from 'flowbite-svelte'
 	import { getApiData } from '$lib/services/getData'
 	import { storeUser } from '$lib/stores'
-	import { sendErrorNotification, sendSuccessNotification } from '$lib/stores/toast'
-	import { merge } from 'lodash-es'
+	import { sendErrorNotification } from '$lib/stores/toast'
 	import Icon from '$lib/components/common/Icon.svelte'
+	import {
+		getJsonSchema,
+		getSchemaComputed,
+		handleSubmitForm
+	} from '$lib/helpers/formbuilder/index'
 
 	export let data: any
-
 	const widget: any = getContext('widget')
 	const dashboard = getContext<Writable<any>>('dashboard')
 	const widgetActions = getContext<Writable<any[]>>('widgetActions')
@@ -25,42 +28,18 @@
 	const token = $storeUser?.token
 	let responseServer = null
 
-	function getSchemaComputed(jsonSchema: Record<string, any>) {
-		if ($widget?.params?.model?.schema?.$withoutDefs && jsonSchema?.$defs) {
-			delete jsonSchema.$defs
-		}
-
-		return merge({}, jsonSchema, $widget?.params?.model?.schema || {})
-	}
-
-	function handleSubmitForm(handleValidateForm: any, type: string) {
-		const payload = handleValidateForm()
-		console.log(payload)
-		if (!Array.isArray(payload)) {
-			handleSubmit(payload, type)
-		} else {
-			sendErrorNotification('There has been a problem...')
-		}
-	}
-
-	async function handleSubmit(payload: any, type: string) {
+	async function handleSubmitFormLocal(handleValidateForm: any, type: string) {
 		const endpoint = `${schema?.endpoint || meta}`
 
-		let url = `${baseUrl}/${endpoint}`
-		let method = 'PUT'
-		let message = 'Successfully created'
-		let callback = $widget.callbackNew
+		const response = await handleSubmitForm(handleValidateForm, type, $widget, {
+			baseUrl,
+			endpoint
+		})
 
-		const dataModel = await getApiData(url, method, payload)
-
-		if (dataModel) {
-			sendSuccessNotification(dataModel?.message || message)
+		if (response) {
 			if ($widget?.params?.model?.responseAlert) {
-				responseServer = dataModel?.message || message
+				responseServer = response.message
 			}
-		} else {
-			console.log('Error here', dataModel)
-			sendErrorNotification('There has been a problem...')
 		}
 	}
 
@@ -72,76 +51,16 @@
 
 		meta = $widget.params.model && $widget.params.model.meta ? $widget.params.model.meta : null
 
-		const jsonSchema = await getApiData(`${baseUrl}/${meta}:meta`, 'GET')
+		const prepareJsonSchema = await getApiData(`${baseUrl}/${meta}:meta`, 'GET')
+		if (prepareJsonSchema) {
+			const jsonSchema = await getJsonSchema(prepareJsonSchema, $widget, { baseUrl, token })
 
-		if (jsonSchema) {
-			jsonSchema['noHeader'] = true
-			title = `${jsonSchema?.title}`
-			description = jsonSchema?.description
+			schema = getSchemaComputed(jsonSchema, $widget)
 
-			Object.keys(jsonSchema.properties).map((property) => {
-				if (
-					jsonSchema.properties[property]?.$ref?.api &&
-					jsonSchema.properties[property]?.type === 'object'
-				) {
-					jsonSchema.properties[property].type = 'select'
-
-					jsonSchema.properties[property].$ref['_fetch'] = {
-						baseUrl: `${baseUrl}/${
-							$widget?.params?.model?.schema?.properties &&
-							$widget?.params?.model?.schema?.properties[property]
-								? $widget?.params?.model?.schema?.properties[property]?.$ref?.url
-								: 'api/v1/'
-						}`,
-						headers: {
-							authorization: `Bearer ${token}`
-						}
-					}
-
-					delete jsonSchema.properties[property]?.$ref?.$ref
-				}
-
-				if (jsonSchema.properties[property]?.enum_type) {
-					jsonSchema.properties[property].items = jsonSchema.properties[property]?.enum_type
-					delete jsonSchema.properties[property]?.enum_type
-				}
-
-				if (
-					jsonSchema.properties[property]?.type === 'text' ||
-					jsonSchema.properties[property]?.['ui:widget'] === 'textarea'
-				) {
-					jsonSchema.properties[property].type = 'string'
-					jsonSchema.properties[property]['format'] = 'textarea'
-				}
-
-				if (jsonSchema.properties[property]?.type === 'datetime') {
-					jsonSchema.properties[property].attrs.visible = false
-				}
-
-				if (jsonSchema.properties[property]?.['ui:widget'] === 'ImageUploader') {
-					// TODO: improve
-					jsonSchema.properties[property].type = 'upload'
-
-					delete jsonSchema.properties[property].attrs.placeholder
-
-					jsonSchema.properties[property].attrs['fetching'] = {
-						url: `${baseUrl}/services/files/static/images/badges/`,
-						method: 'PUT',
-						payload: 'file_name',
-						headers: {
-							authorization: `Bearer ${token}`
-						}
-					}
-
-					if (jsonSchema.properties[property]?.['ui:help'])
-						jsonSchema.properties[property].attrs.help =
-							jsonSchema.properties[property]?.['ui:help']
-				}
-			})
-
-			schema = getSchemaComputed(jsonSchema)
-
-			console.log(JSON.stringify(schema))
+			if (schema) {
+				title = `${jsonSchema?.title}`
+				description = jsonSchema?.description
+			}
 		} else {
 			sendErrorNotification('The form could not be loaded')
 		}
@@ -178,7 +97,7 @@
 						<div class="flex items-end justify-end">
 							<Button
 								class="btn-pull-left mt-2 w-full	 rounded text-sm"
-								on:click={() => handleSubmitForm(handleValidateForm, 'formSaved')}
+								on:click={() => handleSubmitFormLocal(handleValidateForm, 'formSaved')}
 							>
 								<Icon icon="tabler:plus" classes="mr-2" />
 
