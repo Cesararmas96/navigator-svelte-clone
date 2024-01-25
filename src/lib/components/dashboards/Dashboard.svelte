8db07661893a6cba406ba38fbdd022b118b3d761
Ricardo Giannotti <gianok@gmail.com>
2024-01-25 16:14:41 -0400

    Fix widget menu bugs

--- src/lib/components/dashboards/Dashboard.svelte
+++ src/lib/components/dashboards/Dashboard.svelte
@@ -134,7 +134,6 @@
 	}
 	$: handleCollapse = (item: any, collapse: boolean) => {
 		if (isMobileDevice()) return
-		console.log('collapse')
 		$storeDashboard.gridItems = resizeCollapseItem(item, $storeDashboard.gridItems, collapse)
 		gridController.gridParams.updateGrid()
 		$storeDashboard.gridItems = [...$storeDashboard.gridItems]
