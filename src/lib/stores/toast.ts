1c5dce2ec0dbf20f11b8b5a182187ff8d0abf781
Ricardo Giannotti <gianok@gmail.com>
2024-02-05 13:32:22 -0400

    fix widget location timestamp bug

--- src/lib/stores/toast.ts
+++ src/lib/stores/toast.ts
@@ -1,53 +1,54 @@
-import { ToastType, type Toast } from "$lib/interfaces/Toast";
-import { writable } from "svelte/store";
+import { ToastType, type Toast } from '$lib/interfaces/Toast'
+import { writable } from 'svelte/store'
 
-export const toasts = writable<Toast[]>([]);
+export const toasts = writable<Toast[]>([])
 
 export const sendNotification = (toast: Toast) => {
-  toast.dismissable = false
-  toasts.update((all) => [{...toast }, ...all]);
-  if (toast.timeout) setTimeout(() => dismissNotification(toast.id), toast.timeout);
-};
+	toast.dismissable = false
+	toasts.update((all) => [{ ...toast }, ...all])
+	if (toast.timeout) setTimeout(() => dismissNotification(toast.id), toast.timeout)
+}
 
 export const dismissNotification = (id: number) => {
-  toasts.update((all) => all.filter((t: Toast) => t.id !== id));
-};
-
-const generateId = () => Math.floor(Math.random() * 10000);
-
-export const sendSuccessNotification = (message: string, timeout: number = 3000) => {
-  sendNotification({
-    id: generateId(),
-    type: ToastType.SUCCESS,
-    message,
-    timeout
-  });
+	toasts.update((all) => all.filter((t: Toast) => t.id !== id))
 }
 
-export const sendWarningNotification = (message: string, timeout: number = 3000) => {
-  sendNotification({
-    id: generateId(),
-    type: ToastType.WARNING,
-    message,
-    timeout
-  });
+const generateId = () => Math.floor(Math.random() * 10000)
+
+export const sendSuccessNotification = (message: string, timeout = 3000) => {
+	sendNotification({
+		id: generateId(),
+		type: ToastType.SUCCESS,
+		message,
+		timeout
+	})
 }
 
-export const sendErrorNotification = (error: any, timeout: number = 3000) => {
-  let message = error.message || error
-  sendNotification({
-    id: generateId(),
-    type: ToastType.ERROR,
-    message,
-    timeout
-  });
+export const sendWarningNotification = (message: string, timeout = 3000) => {
+	sendNotification({
+		id: generateId(),
+		type: ToastType.WARNING,
+		message,
+		timeout
+	})
 }
 
-export const sendInfoNotification = (message: string, timeout: number = 3000) => {
-  sendNotification({
-    id: generateId(),
-    type: ToastType.INFO,
-    message,
-    timeout
-  });
+export const sendErrorNotification = (error: any, timeout = 3000) => {
+	const message = error.message || error
+	sendNotification({
+		id: generateId(),
+		type: ToastType.ERROR,
+		message,
+		timeout
+	})
+	console.error(error)
+}
+
+export const sendInfoNotification = (message: string, timeout = 3000) => {
+	sendNotification({
+		id: generateId(),
+		type: ToastType.INFO,
+		message,
+		timeout
+	})
 }
