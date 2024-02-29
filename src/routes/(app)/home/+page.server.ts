b4896f78667c40618831bda4a3b6f0f903a9f459
Ricardo Giannotti <gianok@gmail.com>
2024-02-29 17:39:05 -0400

    Login with 1 program fix bug

--- src/routes/(app)/home/+page.server.ts
+++ src/routes/(app)/home/+page.server.ts
@@ -6,31 +6,26 @@ export const load = async ({ locals, fetch, url }) => {
 	if (import.meta.env.VITE_ADMIN) throw redirect(302, '/admin')
 	const headers = { authorization: `Bearer ${locals.user.token}` }
 
-	try {
-		const programs = await getApiData(
-			`programs`,
-			'POST',
-			{
-				where_cond: {
-					program_slug: locals.user.programs,
-					is_active: true
-				}
-			},
-			{},
-			{ headers },
-			fetch,
-			false
-		)
+	const programs = await getApiData(
+		`programs`,
+		'POST',
+		{
+			where_cond: {
+				program_slug: locals.user.programs,
+				is_active: true
+			}
+		},
+		{},
+		{ headers },
+		fetch,
+		false
+	)
 
-		const programType = await getApiData(`program_type`, 'POST', {}, {}, { headers }, fetch)
+	if (programs.length === 1) throw redirect(302, `/${programs[0].program_slug}`)
 
-		if (programs.length === 1) throw redirect(302, `/${programs[0].program_slug}`)
-		// if (locals.user.next) throw redirect(302, `/${locals.user.next}`)
+	const programType = await getApiData(`program_type`, 'POST', {}, {}, { headers }, fetch)
 
-		const tenant = url.hostname.split('.')[0]
+	const tenant = url.hostname.split('.')[0]
 
-		return { programs, programType, user: locals.user, tenant }
-	} catch (error) {
-		return { error }
-	}
+	return { programs, programType, user: locals.user, tenant }
 }
