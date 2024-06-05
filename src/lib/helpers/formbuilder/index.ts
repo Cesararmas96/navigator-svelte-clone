0404927150981b50c5c7b39210373ee8243bb8be
mendozajoser <mendozajoser70@gmail.com>
2024-06-05 16:19:22 -0500

    empty commit

--- src/lib/helpers/formbuilder/index.ts
+++ src/lib/helpers/formbuilder/index.ts
@@ -349,7 +349,7 @@ export function supportTicket(params) {
 }
 
 export function supportTicketBose(params) {
-	// let message = `${params?.response?.message} <br> Ticket ID	${params?.response?.ticket_number}  `
+	// let message = `${params?.response?.message} <br> Ticket ID	${params?.response?.ticket_number} `
 	const message = `Your report was submitted, and a case was opened under <strong>Ticket ID ${params?.response?.ticket_number}</strong>, please take note of this number in case you want to follow up on this ticket later.`
 
 	return message
